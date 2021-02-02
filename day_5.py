ac2d86276898acde5ad88dc6788e5de10b2d819d
Дима Заикин <predtech1988@gmail.com>
2021-02-03 02:04:33 +0300

    Final version. #8 Jumping over Hurdles with Variable Heights

--- day_5.py
+++ day_5.py
@@ -5,25 +5,27 @@ def turn_around():
 def turn_rigth():
     turn_left()
     turn_left()
-    turn_left()
-    
-def turn_3():
-    turn_left()
-    turn_left()
-    turn_left()
-    turn_left()
-    
+    turn_left()    
+
 def jump_1():
         turn_left()
         move()
         turn_rigth()
+        
+while not(at_goal()):
+    turn_left()
+    if wall_on_right():
+        turn_rigth()
+        jump_1()
+        if front_is_clear():
+            move()
+            turn_rigth()
+            if front_is_clear():
+                while front_is_clear():
+                    move()
+                turn_left()
+
+
+
 
         
-while not(at_goal()):    
-    if front_is_clear():
-        move()
-    elif wall_in_front():
-        jump_1()
-
-    elif wall_on_right():
-        jump_1()
